0332d09da8c8bc1fd28ddf61ab869f3e86c17609
Pranav <pranav@pranavprakash.net>
2014-10-02 11:39:33 +0530

    Fix side menu

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,17 @@
+$(document).ready(function() {
+    /* Menu Expand/Collapse */
+    var layout = $('#layout'),
+        menu = $('#menu'),
+        menuLink = $('#menuLink');
+    menuLink.click(function(evt) {
+        var active = 'active';
+        evt.preventDefault();
+        layout.toggleClass(active);
+        menu.toggleClass(active);
+        menuLink.toggleClass(active);
+    });
+});
+
 /* NProgress */
 $(document).on('page:fetch', function() {
     NProgress.start();
