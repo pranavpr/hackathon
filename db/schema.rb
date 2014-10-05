2015d6abf2e79a61649a27bf61d5417c08badfd7
Pranav <pranav@pranavprakash.net>
2014-10-06 01:22:37 +0530

    Fix d b schema

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 20141005111636) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
